f0ef708194565c1fd7610a4f0f0f79e3eb960739
Ashaypatakare04 <patakareashay@gmail.com>
2026-02-03 06:43:44 -0800

    Add files via upload

---
+++ Experiment_2/Sorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Sorting{
+public static void main(String[] args){
+String[] names={"Ashay","Pratik","Pranav","Soham","Ganesh","Meenakshi","Ratnesh","Ritesh","Siddhesh","Sahil"};
+
+Arrays.sort(names);
+for(String name:names){
+System.out.println(name);
+}
+}
+}
