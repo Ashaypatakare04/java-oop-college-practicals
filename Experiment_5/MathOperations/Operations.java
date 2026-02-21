83e8f281d14860b1da04cb8da985c71b48d2f411
Ashaypatakare04 <patakareashay@gmail.com>
2026-02-21 11:24:55 +0530

    Add files via upload

---
+++ Experiment_5/MathOperations/Operations.java
@@ -0,0 +1,15 @@
+package MathOperations;
+
+public class Operations {
+    public double floorValue(double n) {
+        return Math.floor(n);
+    }
+
+    public double ceilValue(double n) {
+        return Math.ceil(n);
+    }
+
+    public long roundValue(double n) {
+        return Math.round(n);
+    }
+}
