3df9f4da7548c52d83172e5df8c132fa3a3a05af
Ashaypatakare04 <patakareashay@gmail.com>
2026-04-21 09:11:53 +0530

    Version 2.0

--- Experiment_8/Reverse.java
+++ Experiment_8/Reverse.java
@@ -1,14 +1,14 @@
 import javax.swing.*;
 public class Reverse{
 public static void main(String[] args){
-f.setSize(300,150);
-f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-f.setLayout(new java.awt.FlowLayout());
-
 JFrame f=new JFrame("Reverse Number");
 JTextField t=new JTextField(10);
 JButton b=new JButton("Reverse");
 JLabel l=new JLabel();
+
+f.setSize(300,150);
+f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+f.setLayout(new java.awt.FlowLayout());
 
 b.addActionListener(e->{
 String num=t.getText();
